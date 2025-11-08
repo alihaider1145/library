3c1cfa0900b11a31854de763755f3f42e06c6143
alihaider1145 <alihaider15112006@gmail.com>
2025-11-08 23:02:10 +0500

    Merge added books through form into table

--- script.js
+++ script.js
@@ -19,13 +19,9 @@ const myLibrary = [
     }
 ];
 
-const table = document.querySelector('.library-table');
 const tableBody = document.querySelector('.library-table tbody')
-
-console.log(document.querySelector('.library-table'));      // should be <table> element
-console.log(document.querySelector('.library-table tbody')); // should be <tbody> element
-
-
+const form = document.querySelector('.library-form');
+const formBtn = document.querySelector('.formBtn');
 
 function Book(author, title, pages, isRead, ID){
 
@@ -54,5 +50,17 @@ function displayBooks(){
         tableBody.appendChild(row);
     });
 }
+
+form.addEventListener('submit', (e)=>{
+    console.log('The submit event works');
+    e.preventDefault();
+    const newAuthor = document.querySelector('#author').value;
+    const newTitle = document.querySelector('#title').value;
+    const newPages = document.querySelector('#pages').value;
+    const newIsRead = document.querySelector('#isRead').checked;
+    addBookToLibrary(newAuthor, newTitle, newPages, newIsRead);
+    displayBooks();
+    form.reset();
+});
 
 displayBooks();
