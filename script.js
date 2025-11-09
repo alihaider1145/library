aca2112309aeb96236d9f9ad577630cf831198d7
alihaider1145 <alihaider15112006@gmail.com>
2025-11-09 13:50:44 +0500

    Add the Read Button Toggle

--- script.js
+++ script.js
@@ -44,11 +44,39 @@ function displayBooks(){
 
     myLibrary.forEach((rowData)=>{
         const row = document.createElement('tr');
-        let rowArr = Object.values(rowData).slice(0,4); // converts the row obj to arr
+        let rowArr = Object.values(rowData).slice(0,3); // converts the row obj to arr
         rowArr.forEach((cellData)=>{
             const cell = document.createElement('td')
             cell.textContent = cellData;
             row.appendChild(cell);
+        });
+
+        const cell = document.createElement('td')
+        const readBtn = document.createElement('button');
+        readBtn.style.backgroundColor = '#FFFFFF';
+        readBtn.style.width = '90px';
+        readBtn.style.height = '35px';
+        readBtn.style.color = '#111';
+        readBtn.style.border = '1px solid #111';
+        readBtn.style.borderRadius = '4px';
+        readBtn.textContent = (rowData.isRead ? "Read" : "Not Read");
+
+        readBtn.addEventListener('click', ()=>{
+            if(readBtn.textContent === 'Read'){
+                readBtn.textContent = 'Not Read';
+            }
+            else
+                readBtn.textContent = 'Read';
+        });
+
+        readBtn.addEventListener('mouseenter', ()=>{
+            readBtn.style.border = '1px solid grey';
+            readBtn.style.cursor = 'pointer';
+        });
+
+        readBtn.addEventListener('mouseleave', ()=>{
+            readBtn.style.border = '1px solid #111';
+            readBtn.style.cursor = 'default';
         });
 
         const delBtn = document.createElement('button');
@@ -61,10 +89,12 @@ function displayBooks(){
 
         delBtn.addEventListener('mouseenter', ()=>{
             delBtn.style.backgroundColor = 'Maroon';
+            delBtn.style.cursor = 'pointer';
         });
 
         delBtn.addEventListener('mouseleave', ()=>{
             delBtn.style.backgroundColor = 'Red';
+            delBtn.style.cursor = 'default';
         });
 
         delBtn.addEventListener('click', (e)=>{
@@ -74,6 +104,8 @@ function displayBooks(){
             e.target.parentNode.remove();
         });
 
+        row.appendChild(cell);
+        cell.appendChild(readBtn);
         row.appendChild(delBtn);
 
         tableBody.appendChild(row);
